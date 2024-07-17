143110790a1960d0979b804504831dfbe7095f39
Lisa <dukalisakhanya@gmail.com>
2024-07-17 12:12:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Counter from './Counter';
+
+function App (){
+  return (
+    <div className= "App">
+      <Counter/>
+    </div>
+  );
+}
 
 function App() {
   return (
