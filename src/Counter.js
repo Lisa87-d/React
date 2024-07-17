488d5cffb09790e45f533f97bb92c3753b616304
Lisa <dukalisakhanya@gmail.com>
2024-07-17 14:57:21 +0200

    first commit

--- src/Counter.js
+++ src/Counter.js
@@ -2,6 +2,13 @@ import React , {useState , useEffect } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    useEffect(() => {
+        console.log('Count changed: ${count}');
+    }, [count]
+
+    );
+    
+    
     return (
         <div>
             <h1>Count: {count}</h1>
