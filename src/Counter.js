9e206ccdaf5413f7e2b740f9f15907400520b1b6
Lisa <dukalisakhanya@gmail.com>
2024-07-23 11:14:18 +0200

    changed the semi-colon

--- src/Counter.js
+++ src/Counter.js
@@ -4,9 +4,9 @@ const Counter = () => {
     const [count, setCount] = useState(0);
     useEffect(() => {
         console.log('Count changed: ${count}');
-    }, [count]
+    }, [count]);
 
-    );
+
     
     
     return (
