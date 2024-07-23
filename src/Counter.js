b3243e343fdf373cd748868ac0c9199bd0fd8949
Lisa <dukalisakhanya@gmail.com>
2024-07-23 12:10:44 +0200

    changed the positive to negative

--- src/Counter.js
+++ src/Counter.js
@@ -3,7 +3,7 @@ import React , {useState , useEffect } from 'react';
 const Counter = () => {
     const [count, setCount] = useState(0);
     useEffect(() => {
-        console.log('Count changed: ${count}');
+        console.log(`Count changed: ${count}`);
     }, [count]);
 
 
@@ -13,7 +13,7 @@ const Counter = () => {
         <div>
             <h1>Count: {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increase</button>
-            <button onClick={() => setCount(count + 1)}>Decrease</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
 
 
         </div>
