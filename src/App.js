af37758f6246afc3a0d803039d1962e0da2c2d39
Lisa <dukalisakhanya@gmail.com>
2024-07-23 13:10:16 +0200

    changed className from App2 to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 
 import React from 'react';
 import Counter from './Counter';
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
 
 function App (){
   return (
-    <div className= "App2">
+    <div className= "App">
       <Counter/>
     </div>
   );
 }
 
+
+const App =() => {
+  return(
+    <div className="App">
+      <Home/>
+
+    </div>
+  );
+
+};
 
 
 export default App;
