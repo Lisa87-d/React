52645748021a9426c289745d7f18a4771c46415e
Lisa <dukalisakhanya@gmail.com>
2024-07-23 13:38:48 +0200

    removed functin app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
 import React from 'react';
 import Counter from './Counter';
 
-import './App.css';
-import Home from './components/Home';
 
-function App (){
+
+
+function App () {
   return (
     <div className= "App">
       <Counter/>
     </div>
-  );
-}
+  ); 
+};
+  
 
 
 
