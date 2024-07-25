cce64d4767e9b221fdbf2d49b615755aa87f42c1
Lisa <dukalisakhanya@gmail.com>
2024-07-25 12:35:13 +0200

    changed app to application

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Counter from './Counter';
 import './App.css';
 import Home from './Components/Home';
-import Navbar from '.Components/Navbar';
+import Navbar from './Components/Navbar';
 import Contact from './Components/Contact';
 import About from './Components/About';
 
